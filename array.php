61736da711c38c14e74a232998cc192b3de59268
kyomel <michaellapandio04@gmail.com>
2020-08-02 20:45:57 +0700

    [kyomel] php-learning with wegodev

---
+++ array.php
@@ -0,0 +1,17 @@
+<?php
+
+	$nama[0] = "Rudi";
+	$nama[1] = "Desta";
+	$nama[2] = "Mike";
+	
+	$jenisKelamin = array("C" => "Cewek", "L" => "Laki-Laki");
+	
+	$olahraga = array("Bola", "Basket", "Badminton", "Tenis Meja");
+	
+	echo $nama[1];
+	echo "<br/>";
+	echo $jenisKelamin["C"];
+	echo "<br/>";
+	echo $olahraga[2];
+	
+?>
---
+++ variable.php
@@ -0,0 +1,12 @@
+<?php
+
+	$nilai1 = 10;
+	$nilai2 = 30;
+	$nilai3 = $nilai1 + $nilai2;
+	
+	echo "Variable Nilai = $nilai1 <br/>";
+	echo "Variable Nilai = $nilai2 <br/>";
+	echo "Variable Nilai 3 adalah hasil dari nilai 1 + nilai 2 = $nilai3";
+	
+	
+?>
